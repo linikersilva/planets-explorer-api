250994f5abbda4b3e3033f3928849e68428be326
Liniker Silva <linikergabrieldasilva@gmail.com>
2024-09-02 11:56:47 -0300

    Initial commit

---
+++ src/main/java/org/example/planetsexplorer/PlanetsExplorerApplication.java
@@ -0,0 +1,13 @@
+package org.example.planetsexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetsExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetsExplorerApplication.class, args);
+	}
+
+}
